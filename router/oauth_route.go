637c4abc858ab10ed055b4d69c6b2b8820827930
Novad <novadwynt28@gmail.com>
2025-03-19 10:31:26 +0700

    feat: add middlware

--- router/oauth_route.go
+++ router/oauth_route.go
@@ -3,14 +3,18 @@ package router
 import (
 	"learn_o_auth-project/api"
 	"learn_o_auth-project/api/controller"
+	"learn_o_auth-project/helper"
 	"learn_o_auth-project/presentation"
 
 	"github.com/gin-gonic/gin"
 )
 
 func OAuthRoutes(r *gin.Engine) {
+	midleware := helper.RequireAccessToken
+
 	r.GET("/", presentation.LoginPage)
-	r.GET("/home", presentation.HomePage)
+	r.GET("/home", midleware, presentation.HomePage)
 	r.GET("/login", controller.HandleGoogleLogin)
+	r.GET("/logout", controller.HandleLogOut)
 	r.GET("/callback", api.InitializeAuthController().HandleGoogleCallback)
 }
