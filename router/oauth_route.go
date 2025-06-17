95c39f4d1eee77eb54d18fcbfb1797b9f5e8dba7
Novad <novadwynt28@gmail.com>
2025-06-17 19:16:28 +0700

    refactor OAuthRoutes: remove CORS middleware configuration

--- router/oauth_route.go
+++ router/oauth_route.go
@@ -4,10 +4,7 @@ import (
 	"go_auth-project/api"
 	"go_auth-project/api/controller"
 	"go_auth-project/helper"
-	"os"
-	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,14 +12,6 @@ func OAuthRoutes(r *gin.Engine) {
 	authMidleware := helper.AuthMiddleware
 	guestMiddleware := helper.GuestMiddleware
 	authController := api.AuthInjector()
-
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{os.Getenv("FRONTEND_BASE_URL")},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "Refresh-token", "Signed-token", "Oauth-State"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 
 	r.POST("/sign-up", guestMiddleware, authController.HandleSignUp)
 	r.POST("/login", guestMiddleware, authController.HandleLogin)
