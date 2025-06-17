e402a872875a0650f0e7c7704c8d569093558642
Novad <novadwynt28@gmail.com>
2025-06-17 19:18:02 +0700

    refactor OAuthRoutes: configure CORS middleware for enhanced security

--- router/oauth_route.go
+++ router/oauth_route.go
@@ -4,7 +4,10 @@ import (
 	"go_auth-project/api"
 	"go_auth-project/api/controller"
 	"go_auth-project/helper"
+	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +15,14 @@ func OAuthRoutes(r *gin.Engine) {
 	authMidleware := helper.AuthMiddleware
 	guestMiddleware := helper.GuestMiddleware
 	authController := api.AuthInjector()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{os.Getenv("FRONTEND_BASE_URL")},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "Refresh-token", "Signed-token", "Oauth-State"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.POST("/sign-up", guestMiddleware, authController.HandleSignUp)
 	r.POST("/login", guestMiddleware, authController.HandleLogin)
